9e84a8f025d370ae2d327676813596c8975ede7e
ESPersonnel <w.ndiba@gmail.com>
2024-12-09 18:50:32 +0300

    Prepare files for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ghpages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Distance-Visibility-Calculator/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
